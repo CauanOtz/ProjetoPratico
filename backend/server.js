2e553322cad36236add5f604dd3fba11fce3a454
CAUAN ORTIZ <0031432412004@lti.fatecso.edu.br>
2024-10-10 17:14:32 -0300

    mongo conectado

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './config/db.js';
 
 dotenv.config();
 
@@ -16,7 +17,9 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
- app.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`);
- });
+connectDB().then(() =>{
+    app.listen(PORT, () => {
+       console.log(`Server is running at port ${PORT}`);
+    });
+});
 
